76dc4f6d17ff2cea6dc5aa23243c14686a9c3492
Fujimoto <kazuki.fujimoto@jbgoode.jp>
2026-01-26 10:17:31 +0900

    modify key tables

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -19,46 +19,31 @@ config.initial_cols = 220
 ----------------------------------------------------
 -- Tab
 ----------------------------------------------------
--- タイトルバーを非表示
 config.window_decorations = "RESIZE"
--- タブバーの表示
 config.show_tabs_in_tab_bar = true
 config.tab_bar_at_bottom = true
--- タブが一つの時は非表示
 config.hide_tab_bar_if_only_one_tab = true
--- falseにするとタブバーの透過が効かなくなる
--- config.use_fancy_tab_bar = false
 
--- タブバーの透過
 config.window_frame = {
   inactive_titlebar_bg = "none",
   active_titlebar_bg = "none",
 }
 
--- タブバーを背景色に合わせる
 config.window_background_gradient = {
   colors = { "#0a0a0a" },
 }
 
--- タブの追加ボタンを非表示
 config.show_new_tab_button_in_tab_bar = false
--- nightlyのみ使用可能
--- タブの閉じるボタンを非表示
 config.show_close_tab_button_in_tabs = false
 
--- タブ同士の境界線を非表示
 config.colors = {
   tab_bar = {
     inactive_tab_edge = "none",
   },
-
   cursor_bg = "#F1009A",
 }
 
--- タブの形をカスタマイズ
--- タブの左側の装飾
 local SOLID_LEFT_ARROW = wezterm.nerdfonts.ple_lower_right_triangle
--- タブの右側の装飾
 local SOLID_RIGHT_ARROW = wezterm.nerdfonts.ple_upper_left_triangle
 
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
@@ -85,59 +70,56 @@ wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_wid
 end)
 
 ----------------------------------------------------
--- keybinds
+-- Keybindings & CopyMode
 ----------------------------------------------------
 config.disable_default_key_bindings = true
-config.keys = require("keybinds").keys
-config.key_tables = require("keybinds").key_tables
-table.insert(config.keys, transparency.key)
 config.leader = { key = "q", mods = "CTRL", timeout_milliseconds = 2000 }
 
-----------------------------------------------------
+local keys = require("keybinds").keys or {}
+local key_tables = require("keybinds").key_tables or {}
+
+table.insert(keys, {
+  key = 'X',
+  mods = 'CTRL|SHIFT',
+  action = act.ActivateCopyMode,
+})
+table.insert(keys, transparency.key)
+
 -- CopyMode
-----------------------------------------------------
-config.keys = {
+key_tables.copy_mode = {
+  { key = 'Escape', mods = 'NONE',  action = act.CopyMode 'Close' },
+  { key = 'q',      mods = 'NONE',  action = act.CopyMode 'Close' },
+
+  { key = 'h',      mods = 'NONE',  action = act.CopyMode 'MoveLeft' },
+  { key = 'j',      mods = 'NONE',  action = act.CopyMode 'MoveDown' },
+  { key = 'k',      mods = 'NONE',  action = act.CopyMode 'MoveUp' },
+  { key = 'l',      mods = 'NONE',  action = act.CopyMode 'MoveRight' },
+  { key = 'w',      mods = 'NONE',  action = act.CopyMode 'MoveForwardWord' },
+  { key = 'b',      mods = 'NONE',  action = act.CopyMode 'MoveBackwardWord' },
+  { key = 'e',      mods = 'NONE',  action = act.CopyMode 'MoveForwardWordEnd' },
+  { key = '0',      mods = 'NONE',  action = act.CopyMode 'MoveToStartOfLine' },
+  { key = '$',      mods = 'NONE',  action = act.CopyMode 'MoveToEndOfLineContent' },
+  { key = '^',      mods = 'NONE',  action = act.CopyMode 'MoveToStartOfLineContent' },
+  { key = 'g',      mods = 'NONE',  action = act.CopyMode 'MoveToScrollbackTop' },
+  { key = 'G',      mods = 'SHIFT', action = act.CopyMode 'MoveToScrollbackBottom' },
+  { key = 'd',      mods = 'CTRL',  action = act.CopyMode { MoveByPage = 0.5 } },
+  { key = 'u',      mods = 'CTRL',  action = act.CopyMode { MoveByPage = -0.5 } },
+  { key = 'v',      mods = 'NONE',  action = act.CopyMode { SetSelectionMode = 'Cell' } },
+  { key = 'V',      mods = 'NONE',  action = act.CopyMode { SetSelectionMode = 'Line' } },
+  { key = 'v',      mods = 'CTRL',  action = act.CopyMode { SetSelectionMode = 'Block' } },
+
   {
-    key = 'X',
-    mods = 'CTRL|SHIFT',
-    action = act.ActivateCopyMode,
+    key = 'y',
+    mods = 'NONE',
+    action = act.Multiple {
+      { CopyTo = 'ClipboardAndPrimarySelection' },
+      { CopyMode = 'Close' },
+    }
   },
+  { key = 'Enter', mods = 'NONE', action = act.CopyMode 'ClearSelectionMode' },
 }
 
-config.key_tables = {
-  copy_mode = {
-    { key = 'Escape', mods = 'NONE',  action = act.CopyMode 'Close' },
-    { key = 'q',      mods = 'NONE',  action = act.CopyMode 'Close' },
-
-    -- 移動
-    { key = 'h',      mods = 'NONE',  action = act.CopyMode 'MoveLeft' },
-    { key = 'j',      mods = 'NONE',  action = act.CopyMode 'MoveDown' },
-    { key = 'k',      mods = 'NONE',  action = act.CopyMode 'MoveUp' },
-    { key = 'l',      mods = 'NONE',  action = act.CopyMode 'MoveRight' },
-    { key = 'w',      mods = 'NONE',  action = act.CopyMode 'MoveForwardWord' },
-    { key = 'b',      mods = 'NONE',  action = act.CopyMode 'MoveBackwardWord' },
-    { key = 'e',      mods = 'NONE',  action = act.CopyMode 'MoveForwardWordEnd' },
-    { key = '0',      mods = 'NONE',  action = act.CopyMode 'MoveToStartOfLine' },
-    { key = '$',      mods = 'NONE',  action = act.CopyMode 'MoveToEndOfLineContent' },
-    { key = 'g',      mods = 'NONE',  action = act.CopyMode 'MoveToStartOfViewport' },
-    { key = 'G',      mods = 'SHIFT', action = act.CopyMode 'MoveToEndOfViewport' },
-
-    -- 選択モードの切り替え
-    { key = 'v',      mods = 'NONE',  action = act.CopyMode { SetSelectionMode = 'Cell' } },
-    { key = 'v',      mods = 'SHIFT', action = act.CopyMode { SetSelectionMode = 'Line' } },
-    { key = 'v',      mods = 'CTRL',  action = act.CopyMode { SetSelectionMode = 'Block' } },
-
-    -- コピー
-    {
-      key = 'y',
-      mods = 'NONE',
-      action = act.Multiple {
-        { CopyTo = 'ClipboardAndPrimarySelection' },
-        { CopyMode = 'Close' },
-      }
-    },
-    { key = 'Enter', mods = 'NONE', action = act.CopyMode 'ClearSelectionMode' },
-  },
-}
+config.keys = keys
+config.key_tables = key_tables
 
 return config
